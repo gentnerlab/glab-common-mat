e97bc4a5e7ea10140beb08d1eda954cede4072d4
Bradley Theilman <bradtheilman@gmail.com>
2015-09-25 13:40:16 -0700

    Added matlab routines to deal with toefiles coming from klustakwik. These include routines to generate the toefiles and make spike rasters

---
+++ toefile_utils/make_raster_all_cells.m
@@ -0,0 +1,46 @@
+clear
+close all
+
+% Make a spike raster for a given cell
+
+datafile = 'st1215_cat_P01_S01_2ndPen_20150915T165826';
+
+load(strcat(datafile, '.mat'));
+fs = 31250.0;
+
+
+for unit_index = 1:31
+    
+    disp(unit_index)
+    % Get the data for the chose cell
+    unit_data = toedata{unit_index, 1};
+    nstims = length(unit_data.stims);
+    
+    figure();
+    for stimnum = 1:nstims
+        subplot(7, 7, stimnum)
+        
+        stim_data = unit_data.stims{stimnum, 1};
+        stim_end_secs = double(stim_data.stim_end_times - stim_data.stim_start_times)/fs;
+        ntrials = stim_data.ntrials;
+        for trialnum = 1:ntrials
+            ys = [0 + trialnum, 1+trialnum];
+            if ~isempty(stim_data.toes{trialnum, 1})
+                for spikenum = 1:length(stim_data.toes{trialnum, 1})
+                    line([stim_data.toes{trialnum, 1}(spikenum), stim_data.toes{trialnum, 1}(spikenum)], ys);
+                end
+            end
+            xlim([-2, stim_end_secs(trialnum)+2])
+            ylim([0, ntrials+1]);
+            line([0, 0], [0, ntrials+1], 'Color', 'red');
+            line([stim_end_secs(trialnum), stim_end_secs(trialnum)], [0, ntrials+1], 'Color', 'red');
+        end
+    end
+    fig = gcf;
+    fig.PaperPositionMode = 'auto';
+    figfilename = strcat(datafile, '_cell', num2str(unit_index));
+    print(figfilename, '-dpng', '-r0');
+    close all
+    
+    
+end
---
+++ toefile_utils/make_raster_stim.m
@@ -0,0 +1,37 @@
+clear
+close all
+
+% Make a spike raster for a given cell
+
+
+load('st1215_cat_P01_S01_2ndPen_fixalignment_20150924T141526.mat');
+unit_index = 27;
+fs = 31250.0
+
+% Get the data for the chose cell
+stimnum = 49
+
+nstims = 49;
+
+figure();
+for unit_index= 1:31
+    subplot(8, 4, unit_index)
+
+    unit_data = toedata{unit_index, 1};
+    stim_data = unit_data.stims{stimnum, 1};
+    stim_end_secs = double(stim_data.stim_end_times - stim_data.stim_start_times)/fs;
+    ntrials = stim_data.ntrials;
+    for trialnum = 1:ntrials
+        ys = [0 + trialnum, 1+trialnum];
+        if ~isempty(stim_data.toes{trialnum, 1})
+           for spikenum = 1:length( stim_data.toes{trialnum, 1})
+           line([stim_data.toes{trialnum, 1}(spikenum), stim_data.toes{trialnum, 1}(spikenum)], ys);
+           end
+        end
+        xlim([-2, stim_end_secs(trialnum)+2])
+        ylim([0, ntrials+1]);
+        line([0, 0], [0, ntrials+1], 'Color', 'red');
+        line([stim_end_secs(trialnum), stim_end_secs(trialnum)], [0, ntrials+1], 'Color', 'red');
+    end
+end
+
