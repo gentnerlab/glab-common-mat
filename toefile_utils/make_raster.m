0115474303686c8bcd104fbc8507c9f9a559fb06
Bradley Theilman <bradtheilman@gmail.com>
2015-09-28 22:40:20 -0700

    Can now change colors of plot components
    Just cause.

--- toefile_utils/make_raster.m
+++ toefile_utils/make_raster.m
@@ -20,9 +20,13 @@ unit_index = 9;
 unit_data = toedata{unit_index, 1};
 nstims = length(unit_data.stims);
 
+% Plotting parameters
+spike_color = 'blue';
+stim_color = 'red';
+
 figure();
 for stimnum = 1:nstims
-    subplot(7, 7, stimnum)
+    subplot(7, 6, stimnum)
     stim_data = unit_data.stims{stimnum, 1};
     stim_end_secs = double(stim_data.stim_end_times - stim_data.stim_start_times)/fs;
     ntrials = stim_data.ntrials;
@@ -30,13 +34,13 @@ for stimnum = 1:nstims
         ys = [trialnum-1, trialnum];
         if ~isempty(stim_data.toes{trialnum, 1})
            for spikenum = 1:length( stim_data.toes{trialnum, 1})
-           line([stim_data.toes{trialnum, 1}(spikenum), stim_data.toes{trialnum, 1}(spikenum)], ys);
+           line([stim_data.toes{trialnum, 1}(spikenum), stim_data.toes{trialnum, 1}(spikenum)], ys, 'Color', spike_color);
            end
         end
         xlim([-2, stim_end_secs(trialnum)+2])
         ylim([0, ntrials]);
-        line([0, 0], [0, ntrials+1], 'Color', 'red');
-        line([stim_end_secs(trialnum), stim_end_secs(trialnum)], [0, ntrials+1], 'Color', 'red');
+        line([0, 0], [0, ntrials+1], 'Color', stim_color);
+        line([stim_end_secs(trialnum), stim_end_secs(trialnum)], [0, ntrials+1], 'Color', stim_color);
     end
 end
 
